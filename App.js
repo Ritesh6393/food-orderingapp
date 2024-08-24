8e71ff95b20f0ef1307557c10dfd02fefdb806ff
Ritesh Rai <riteshrai9236@gmail.com>
2024-08-25 00:13:44 +0530

    created restaurant card

--- App.js
+++ App.js
@@ -21,10 +21,49 @@ const Header = ()=>{
     )
 }
 
+const RestaurantCard=()=>{
+    return(
+        <div className='res-card'>
+            
+            <img className='res-logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS2yb943N0rzsn4zkcHJs5GpBbtKpmRii_VA&s'></img>
+
+            <h3 className='res-name'>Meghna Foods</h3>
+            <h5>Biryani,Hyderabadi Biryani,North Indian</h5>
+            <div className='rating-time'>
+                <span>4.4 Star</span>
+                <span>38 Mins</span>
+            </div>
+        </div>
+    )
+}
+
+const Body=()=>{
+    return(
+        <div className='body'>
+            <div className='search'>
+                <input type="text" placeholder="Search Restaurants..." />
+            </div>
+            <div className='res-container'>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
 const AppLayout =() => {
     return(
     <div className='app'>
         <Header/>
+        <Body/>
     </div>
     )
 }
