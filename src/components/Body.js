9412c13b05d145f120db465d3fa1184dbb764b69
Ritesh Rai <riteshrai9236@gmail.com>
2024-08-30 11:37:04 +0530

    added filter functionality for restuarants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,49 +1,54 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import ShimmerUi from "./ShimmerUI";
 
-const Body=()=>{
-    const[listOfRest,setListOfRest]=useState([]);
+const Body = () => {
+    const [listOfRest, setListOfRest] = useState([]);
+    const [searchedInputValue,setSearchedInputVlaue]=useState("");
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
-    },[])
+    }, []);
 
     const fetchData = async () => {
         const data = await fetch("https://66d0b136181d059277df7063.mockapi.io/api/zippy/retaurantsName");
         const restData = await data.json();
-        
-        
-
-        setListOfRest(restData)
-        
+        setListOfRest(restData);
     };
 
-    if(listOfRest.length===0){
-        return <ShimmerUi/> 
+    if (listOfRest.length === 0) {
+        return <ShimmerUi />;
     }
-    return(
-        <div className='body'>
-            {/* <div className='search'>
-                <input type="text" placeholder="Search Restaurants..." />
-            </div> */}
-            <div className='filter'>
-                <button 
-                onClick={()=>{
-                    const filteredList=listOfRest.filter(res=>res.avgRating>4.5);
 
-                    setListOfRest(filteredList);
-                }}>
+    return (
+        <div className='body'>
+            <div className='filter'>
+                <div className='search-container'>
+                    <input type="text" placeholder="Search Restaurants..." value={searchedInputValue} onChange={(e)=>{
+                        setSearchedInputVlaue(e.target.value);
+                    }}
+                    />
+                    <button className="search-button" onClick={()=>{
+                        const searchedRestList=listOfRest.filter(res=>res.name.toLowerCase().includes(searchedInputValue.toLocaleLowerCase()))
+
+                        setListOfRest(searchedRestList);
+                    }}>Search</button>
+                </div>
+                <button className="filter-btn"
+                    onClick={() => {
+                        const filteredList = listOfRest.filter(res => res.avgRating > 4.5);
+                        setListOfRest(filteredList);
+                    }}>
                     Top Rated Restaurants
                 </button>
             </div>
             <div className='res-container'>
-                {listOfRest.map((restaurant)=>{
-                    return(<RestaurantCard key={restaurant.id} resObject={restaurant}></RestaurantCard>)
+                {listOfRest.map((restaurant) => {
+                    return (<RestaurantCard key={restaurant.id} resObject={restaurant}></RestaurantCard>)
                 })}
             </div>
         </div>
-    )
+    );
 }
 
 export default Body;
